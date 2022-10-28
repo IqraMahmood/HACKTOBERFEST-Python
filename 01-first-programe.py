4f0496540df4ccb9204c0e85230aa548ff1fe7c0
MaryamMahmood001 <116386756+MaryamMahmood001@users.noreply.github.com>
2022-10-28 10:23:06 -0700

    Python Basic programs

---
+++ 01-first-programe.py
@@ -0,0 +1,13 @@
+print(1+3)
+print("Hllo world")
+print("We are learning python")
+
+
+# operators
+print(2//5)
+
+age =input("Enter age")
+print(age, type(age))
+
+name =input("what is your name")
+print(name, type(name))

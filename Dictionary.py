8b01ec776b95d8c302c17fbd6c0791b4033d24e4
Daniel Popescu <danielpops@gmail.com>
2022-10-29 15:55:01 -0700

    Fix typos and add example for merging dictionary

--- Dictionary.py
+++ Dictionary.py
@@ -4,6 +4,14 @@ dic = {1:'a',2:'b',3:'c',4:'d',5:'e'}
 
 print(dic[4])  #d
 
-dic[3]=f
-pritn(dic[3])  #f
+dic[3]='f'
+print(dic[3])  #f
 
+
+# Merge two dictionaries
+
+d1 = {'a': 1, 'b': 2, 'c': {'d': 3}}
+d2 = {'b': 4, 'c': {'d': 5, 'e': 6}}
+
+merged = {**d1, **d2}
+print(merged) # {'a': 1, 'b': 4, 'c': {'d': 5, 'e': 6}}

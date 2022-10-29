9f5f0d7d4b50edffacda7fa18e234c38ce43aa4e
CH SUSHMITA <80975689+sushmita-2002@users.noreply.github.com>
2022-10-29 14:57:53 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+#Dictionary is mutable unordered collection with elements in form of key:value pair that associate key to value.
+
+dic = {1:'a',2:'b',3:'c',4:'d',5:'e'}
+
+print(dic[4])  #d
+
+dic[3]=f
+pritn(dic[3])  #f
+
